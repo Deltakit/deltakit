3d1584847b185c7a3fc27c815c810cfb058584de
Adrien Suau <12374487+nelimee@users.noreply.github.com>
2025-12-16 21:02:31 +0000

    Try out new CI configuration

--- tools/transform_labels.py
+++ tools/transform_labels.py
@@ -7,29 +7,38 @@ import json
 import os
 from typing import Final
 
-DEFAULT_PACKAGES: Final[frozenset[str]] = frozenset(
-    [
-        "deltakit",
-        "deltakit-circuit",
-        "deltakit-core",
-        "deltakit-decode",
-        "deltakit-explorer",
-    ]
-)
+DEFAULT_PACKAGES: Final[dict[str, str]] = {
+    "deltakit": "./",
+    "deltakit-circuit": "./deltakit-circuit",
+    "deltakit-core": "./deltakit-core",
+    "deltakit-decode": "./deltakit-decode",
+    "deltakit-explorer": "./deltakit-explorer",
+}
 
 
-def transform(labels_str: str) -> str:
-    labels = sorted(
-        {label.strip() for label in labels_str.split(",")} & DEFAULT_PACKAGES
+def filter_labels(labels_str: str) -> str:
+    present_labels = sorted(
+        {label.strip() for label in labels_str.split(",")} & DEFAULT_PACKAGES.keys()
     )
-    return json.dumps(labels) if labels else json.dumps(list(DEFAULT_PACKAGES))
+    if not present_labels:
+        present_labels = sorted(DEFAULT_PACKAGES)
+    matrix = [
+        {"project": project, "path": DEFAULT_PACKAGES[project]}
+        for project in present_labels
+    ]
+    return json.dumps(matrix)
 
 
 def main():
     all_labels = os.getenv("ALL_LABELS", "")
 
-    with open(os.getenv("GITHUB_OUTPUT"), "a") as f:
-        f.write(f"projects={transform(all_labels)}\n")
+    github_output_path = os.getenv("GITHUB_OUTPUT")
+    if github_output_path is None:
+        msg = "The environment variable GITHUB_OUTPUT should be set."
+        raise ValueError(msg)
+
+    with open(github_output_path, "a") as f:
+        f.write(f"projects={filter_labels(all_labels)}\n")
 
 
 if __name__ == "__main__":
