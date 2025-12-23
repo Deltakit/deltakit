d5906992cf7c8401cbd8c2f65de722ebd5fd95d5
Adrien Suau <12374487+nelimee@users.noreply.github.com>
2025-12-23 14:50:37 +0000

    Use packaging instead of semver

--- deltakit-circuit/src/deltakit_circuit/_stim_version_compatibility.py
+++ deltakit-circuit/src/deltakit_circuit/_stim_version_compatibility.py
@@ -1,21 +1,21 @@
 import importlib.metadata
 import warnings
 import importlib
-import semver
+from packaging.version import Version
 
 
-def _get_stim_version() -> semver.Version:
+def _get_stim_version() -> Version:
     try:
-        return semver.Version.parse(importlib.metadata.version("stim"))
+        return Version(importlib.metadata.version("stim"))
     except AttributeError:
         warnings.warn(
             "Could not get the current version of 'stim'. Assuming 0.0.1.", stacklevel=1
         )
-        return semver.Version(0, 0, 1)
+        return Version("0.0.1")
 
 
 _INSTALLED_STIM_VERSION = _get_stim_version()
-_LOWEST_STIM_VERSION_WITH_TAG_FEATURE = semver.Version(1, 15)
+_LOWEST_STIM_VERSION_WITH_TAG_FEATURE = Version("1.15")
 
 
 def is_stim_tag_feature_available() -> bool:
