d68ab12739593656af0526973ebcddc0d86369a6
Adrien Suau <12374487+nelimee@users.noreply.github.com>
2025-12-24 11:23:29 +0000

    Better tests

--- deltakit-explorer/tests/analysis/budget/test_post_processing.py
+++ deltakit-explorer/tests/analysis/budget/test_post_processing.py
@@ -22,36 +22,45 @@ class ErrorBudgetingResults:
 
 
 @pytest.fixture
-def error_budgeting_data() -> ErrorBudgetingResults:
+def error_parameters() -> npt.NDArray[numpy.floating]:
+    return numpy.array([5e-4, 4e-3, 5e-3, 5e-3, 5e-3])
+
+
+@pytest.fixture
+def error_names() -> list[str]:
+    return ["1q error", "2q error", "Reset error", "Measurement error", "Readout flip"]
+
+
+@pytest.fixture
+def simulated_error_budgeting_data(error_parameters: npt.NDArray[numpy.floating], error_names: list[str]) -> ErrorBudgetingResults:
     _resources_folder = Path(__file__).parent.parent.parent / "resources"
     _data_file = _resources_folder / "error_budget_data.csv"
     data = pd.read_csv(_data_file)
     return ErrorBudgetingResults(
         data,
-        numpy.array([5e-4, 4e-3, 5e-3, 5e-3, 5e-3]),
-        ["1q error", "2q error", "Reset error", "Measurement error", "Readout flip"],
+        error_parameters, error_names
     )
 
 
-def test_data_matches_csv(error_budgeting_data: ErrorBudgetingResults) -> None:
+def test_data_matches_csv(simulated_error_budgeting_data: ErrorBudgetingResults) -> None:
     """Test that the hard-coded parts in the data fixture agree with the CSV file."""
-    header_names: frozenset[str] = frozenset(error_budgeting_data.data.columns.values)
-    for name in error_budgeting_data.parameter_names:
+    header_names: frozenset[str] = frozenset(simulated_error_budgeting_data.data.columns.values)
+    for name in simulated_error_budgeting_data.parameter_names:
         assert f"noise_{name}" in header_names
 
     for noise_name, noise_value in zip(
-        error_budgeting_data.parameter_names,
-        error_budgeting_data.noise_parameters,
+        simulated_error_budgeting_data.parameter_names,
+        simulated_error_budgeting_data.noise_parameters,
         strict=True,
     ):
-        values = error_budgeting_data.data[f"noise_{noise_name}"]
+        values = simulated_error_budgeting_data.data[f"noise_{noise_name}"]
         assert numpy.any(numpy.isclose(values, noise_value))
 
 
 def test_filter_non_close_noise_parameters(
-    error_budgeting_data: ErrorBudgetingResults,
+    simulated_error_budgeting_data: ErrorBudgetingResults,
 ) -> None:
-    data, parameters, names = error_budgeting_data.to_tuple()
+    data, parameters, names = simulated_error_budgeting_data.to_tuple()
     all_noises_index = [f"noise_{name}" for name in names]
     data_frame = _filter_non_close_noise_parameters(data, parameters, names)
     filtered_columns_data_frame = data_frame[all_noises_index]
@@ -60,10 +69,10 @@ def test_filter_non_close_noise_parameters(
 
 
 def test_filter_non_close_noise_parameters_random(
-    error_budgeting_data: ErrorBudgetingResults,
+    simulated_error_budgeting_data: ErrorBudgetingResults,
     random_generator: numpy.random.Generator,
 ) -> None:
-    data, _, names = error_budgeting_data.to_tuple()
+    data, _, names = simulated_error_budgeting_data.to_tuple()
     all_noises_index = [f"noise_{name}" for name in names]
     random_row_index = random_generator.integers(data.shape[0])
     random_parameters = data[all_noises_index].to_numpy()[random_row_index, :]
@@ -71,3 +80,7 @@ def test_filter_non_close_noise_parameters_random(
     filtered_columns_data_frame = data_frame[all_noises_index]
     for row in filtered_columns_data_frame.to_numpy():
         numpy.testing.assert_allclose(row, random_parameters)
+
+
+def test_compute_lambda_and_stddev_from_results():
+    pass
