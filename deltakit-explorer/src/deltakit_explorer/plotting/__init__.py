68bdb7e3926a85c0fdc901fdf33d3964392cc141
Roland Guichard <roland.guichard@riverlane.com>
2026-03-02 15:33:45 +0000

    Fix imports.

--- deltakit-explorer/src/deltakit_explorer/plotting/__init__.py
+++ deltakit-explorer/src/deltakit_explorer/plotting/__init__.py
@@ -3,8 +3,8 @@
 
 from deltakit_explorer.plotting._lambda import plot_lambda
 from deltakit_explorer.plotting._leppr import plot_logical_error_probability_per_round
-from deltakit_explorer.plotting._visualisation import (
-    correlation_matrix,
+from deltakit_explorer.plotting._correlation_matrix import (
+    correlation_matrix
     defect_diagram,
     defect_rates,
 )
