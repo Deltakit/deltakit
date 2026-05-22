6a9bebc9713ecffba1b35176e492a23a5cd9b2e5
Roland Guichard <roland.guichard@riverlane.com>
2026-05-22 16:44:16 +0100

    Fix test.

--- deltakit-circuit/tests/test_stim_version_compatibility.py
+++ deltakit-circuit/tests/test_stim_version_compatibility.py
@@ -1,11 +1,11 @@
-import deltakit_stim as stim
+import deltakit_stim
 from packaging.version import Version
 
 from deltakit_circuit._stim_version_compatibility import is_stim_tag_feature_available
 
 
 def test_stim_version_compatibility() -> None:
-    current_stim_version = Version(stim.__version__)
+    current_stim_version = Version(deltakit_stim.__version__)
     if current_stim_version >= Version("0.1.2"):
         assert is_stim_tag_feature_available()
     else:
