178447dcc242a7f002ad3796f18f64934be787d7
Roland Guichard <roland.guichard@riverlane.com>
2026-06-04 10:40:33 +0100

    Better version checking.

--- deltakit-circuit/src/deltakit_circuit/_stim_version_compatibility.py
+++ deltakit-circuit/src/deltakit_circuit/_stim_version_compatibility.py
@@ -2,6 +2,8 @@ from importlib.metadata import PackageNotFoundError, version
 
 from packaging.version import Version
 
+_LOWEST_STIM_VERSION_WITH_TAG_FEATURE = Version("0.1.2")
+
 
 def _get_stim_version() -> Version | None:
     try:
@@ -10,12 +12,6 @@ def _get_stim_version() -> Version | None:
         return None
 
 
-_INSTALLED_STIM_VERSION = _get_stim_version()
-_LOWEST_STIM_VERSION_WITH_TAG_FEATURE = Version("0.1.2")
-
-
 def is_stim_tag_feature_available() -> bool:
-    return (
-        _INSTALLED_STIM_VERSION is not None
-        and _INSTALLED_STIM_VERSION >= _LOWEST_STIM_VERSION_WITH_TAG_FEATURE
-    )
+    installed = _get_stim_version()
+    return installed is not None and installed >= _LOWEST_STIM_VERSION_WITH_TAG_FEATURE
