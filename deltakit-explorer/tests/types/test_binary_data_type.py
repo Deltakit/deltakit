9f400f3c6bd7ec031433bbeb00057f3073eedac3
Roland Guichard <roland.guichard@riverlane.com>
2026-06-10 15:15:54 +0100

    Xfail test until bug resolution.

--- deltakit-explorer/tests/types/test_binary_data_type.py
+++ deltakit-explorer/tests/types/test_binary_data_type.py
@@ -201,6 +201,7 @@ def test_measurements_split_detectors_observables_stim():
     assert obs.as_numpy().shape == (2, 1)
 
 
+@pytest.mark.xfail(reason="Bug reported in #278")
 def test_measurements_split_detectors_observables_stim_with_leakage():
     mmts = Measurements([[0, 1, 0, 0], [0, 0, 0, 1]])
     circ = """
