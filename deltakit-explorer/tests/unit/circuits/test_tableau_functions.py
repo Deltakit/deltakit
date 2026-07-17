be410f3212bd2531d9443db0ce8762f48e5b3b11
Roland Guichard <roland.guichard@riverlane.com>
2026-07-17 11:39:13 +0100

    Re-enable tests.

--- deltakit-explorer/tests/unit/circuits/test_tableau_functions.py
+++ deltakit-explorer/tests/unit/circuits/test_tableau_functions.py
@@ -102,9 +102,6 @@ from deltakit_explorer.qpu._circuits._tableau_functions import (
 )
 from deltakit_explorer.qpu._native_gate_set import NativeGateSet
 
-CURRENT_STIM_VERSION = Version(version("stim"))
-STIM_VERSION_V1_13_0 = Version("1.13.0")
-
 
 # define set of compilation dictionaries for testing
 compilation_dict0 = {("+X", "+Z"): ()}
@@ -8919,14 +8916,6 @@ class TestTwoQubitGateCompilationDicts:
         )
         assert stim.Tableau.from_named_gate("CZ") == cz_with_unitaries_tableau
 
-    @pytest.mark.skipif(
-        CURRENT_STIM_VERSION < STIM_VERSION_V1_13_0,
-        reason=(
-            "CZSWAP gate has been introduced in Stim v1.13.0."
-            "See https://github.com/quantumlib/Stim/releases/tag/v1.13.0."
-            f"Current Stim version is {CURRENT_STIM_VERSION}."
-        ),
-    )
     @pytest.mark.parametrize("gate", list(GATE_TO_CZSWAP_DICT.keys()))
     def test_cpswap_to_czswap_dict_compilations_give_equivalent_tableaus(self, gate):
         ub1, ub2, ub3, ub4 = GATE_TO_CZSWAP_DICT[gate]
@@ -8960,14 +8949,6 @@ class TestTwoQubitGateCompilationDicts:
             == czswap_with_unitaries_tableau
         )
 
-    @pytest.mark.skipif(
-        CURRENT_STIM_VERSION < STIM_VERSION_V1_13_0,
-        reason=(
-            "CZSWAP gate has been introduced in Stim v1.13.0."
-            "See https://github.com/quantumlib/Stim/releases/tag/v1.13.0."
-            f"Current Stim version is {CURRENT_STIM_VERSION}."
-        ),
-    )
     @pytest.mark.parametrize("gate", list(CZSWAP_TO_GATE_DICT.keys()))
     def test_czswap_to_cpswap_dict_compilations_give_equivalent_tableaus(self, gate):
         ub1, ub2, ub3, ub4 = CZSWAP_TO_GATE_DICT[gate]
