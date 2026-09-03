2566c500747a31396a6bf6e11ebaf4b59e3169ad
Roland Guichard <roland.guichard@riverlane.com>
2026-09-03 08:51:51 +0100

    Add more package modules to namespace.

--- src/deltakit/compile/__init__.py
+++ src/deltakit/compile/__init__.py
@@ -1,1 +1,9 @@
-from deltakit_compile import *
+from deltakit_compile import (
+    dialects,
+    frontend,
+    noise_models,
+    passes,
+)
+
+# List only public members in `__all__`.
+__all__ = [s for s in dir() if not s.startswith("_")]
